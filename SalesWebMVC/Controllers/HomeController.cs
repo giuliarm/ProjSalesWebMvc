05781ab90e3fbdf1d20817c3b85feaf67a1132e1
Giulia Ramos <giulia.rmiguel011@gmail.com>
2021-08-06 18:55:00 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -6,37 +6,32 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SalesWebMVC.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace SalesWebMVC.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+        public IActionResult About() {
+            ViewData["Message"] = "Sales Web MVC App from C# Course ";
+            ViewData["Professor"]= "Nélio Alves";
+            ViewData["By"] = "Giulia Ramos";
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
+        public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
